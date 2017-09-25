5515e9c872f753a5e6710144995b5b7609208b96
lucia <lucia@DESKTOP-6MG35NH>
2017-09-24 22:59:58 -0300

    Resolução e implementação do formulario do usuario

--- Views/index/menuPopup.php
+++ Views/index/menuPopup.php
@@ -19,34 +19,42 @@
                  <h2>Usuario</h2>  <button class="btnx"> X </button> 
              </div>
              <div class="formbody">
-                 <input type="text" class="" name="celular" placeholder="Celular" />
-                 <input type="text" class="" name="senha" placeholder="Senha" />
+                 
+                 <div class="oper1">                  
+                    <input type="text" class="" name="celular" placeholder="Celular" />
+                    <input type="text" class="" name="senha" placeholder="Senha" />
+                 </div>
+                 
+                 <div class="oper2"> 
+                     <?php
+                        echo 'Area de Erros';
+                     ?>
+                 </div>
+                 
+                 <div class="oper3"> 
+                    <button class="btnoper">My Adventure</button>
+                    <button class="btnoper">Apoio</button>
+                 </div>
+                 
+                  <div class="oper4"> 
+                    <button class="btnoper">My Adventure</button>
+                    <button class="btnoper">Apoio</button>
+                    <button class="btnoper">Administração</button>
+                 </div>
+                 
+                  <div class="oper5"> 
+                    <button class="btnoper">My Adventure</button>
+                    <button class="btnoper">Apoio</button>
+                    <button class="btnoper">Administração</button>
+                    <button class="btnoper">Root</button>
+                 </div>
+                 
              </div>
              <div class="formfooter">
                  <button class="btnlogar">Logar</button><button class="btncad">Cadastrar</button>
              </div>
 
 
-
-
-
-
-
-             <div class="fantasma">
-            
-             <div class="box-btnx">            
-                 <button class="btnx"> X </button> 
-             </div>
-            
-            
-            <div class="m-box">
-                <div class="formimg">IMG</div>  <input type="text" placeholder="Celular" class="focus">
-            </div>
-            <div class="m-box">                  
-                    <div class="formimg">IMG</div> <input type="text" class="form-control" placeholder="Senha">
-            </div>
-            <div class="m-box"><button class="btnlogar">Logar</button><button class="btncad">Cadastrar</button></div>
-        </div>
 
         </div>
     </div>
